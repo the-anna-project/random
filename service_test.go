7c8e350304e3f12227aefaaa89e6a9e1f47d2bbc
Tim Schindler <xh3b4sd@gmail.com>
2016-12-13 23:26:54 +0100

    fixed interface

--- service_test.go
+++ service_test.go
@@ -8,46 +8,73 @@ import (
 	"time"
 )
 
-func Test_RandomService_CreateNBetween_Error_RandFactory(t *testing.T) {
-	newService := New()
-	newService.SetTimeout(10 * time.Millisecond)
-	newService.SetRandFactory(func(randReader io.Reader, max *big.Int) (n *big.Int, err error) {
-		return nil, maskAny(timeoutError)
-	})
+func Test_Service_CreateNMax_Error_RandFactory(t *testing.T) {
+	var err error
+
+	var randomService Service
+	{
+		randomConfig := DefaultConfig()
+		randomConfig.RandFactory = func(randReader io.Reader, max *big.Int) (n *big.Int, err error) {
+			return nil, maskAny(timeoutError)
+		}
+		randomConfig.Timeout = 10 * time.Millisecond
+		randomService, err = New(randomConfig)
+		if err != nil {
+			panic(err)
+		}
+	}
 
 	n := 5
 	max := 10
 
-	_, err := newService.CreateNMax(n, max)
+	_, err = randomService.CreateNMax(n, max)
 	if !IsTimeout(err) {
 		t.Fatal("expected", nil, "got", err)
 	}
 }
 
-func Test_RandomService_CreateNBetween_Error_Timeout(t *testing.T) {
-	newService := New()
-	newService.SetTimeout(20 * time.Millisecond)
-	newService.SetRandFactory(func(randReader io.Reader, max *big.Int) (n *big.Int, err error) {
-		time.Sleep(200 * time.Millisecond)
-		return rand.Int(randReader, max)
-	})
+func Test_Service_CreateNMax_Error_Timeout(t *testing.T) {
+	var err error
+
+	var randomService Service
+	{
+		randomConfig := DefaultConfig()
+		randomConfig.RandFactory = func(randReader io.Reader, max *big.Int) (n *big.Int, err error) {
+			time.Sleep(200 * time.Millisecond)
+			return rand.Int(randReader, max)
+		}
+		randomConfig.Timeout = 20 * time.Millisecond
+		randomService, err = New(randomConfig)
+		if err != nil {
+			panic(err)
+		}
+	}
 
 	n := 5
 	max := 10
 
-	_, err := newService.CreateNMax(n, max)
+	_, err = randomService.CreateNMax(n, max)
 	if !IsTimeout(err) {
 		t.Fatal("expected", nil, "got", err)
 	}
 }
 
-func Test_RandomService_CreateNBetween_GenerateNNumbers(t *testing.T) {
-	newService := New()
+func Test_Service_CreateNMax_GenerateNNumbers(t *testing.T) {
+	var err error
+
+	var randomService Service
+	{
+		randomConfig := DefaultConfig()
+		randomService, err = New(randomConfig)
+		if err != nil {
+			panic(err)
+		}
+	}
 
 	n := 5
 	max := 10
 
-	newRandomNumbers, err := newService.CreateNMax(n, max)
+	newRandomNumbers, err := randomService.CreateNMax(n, max)
 	if err != nil {
 		t.Fatal("expected", nil, "got", err)
 	}
@@ -57,13 +84,22 @@ func Test_RandomService_CreateNBetween_GenerateNNumbers(t *testing.T) {
 	}
 }
 
-func Test_RandomService_CreateNBetween_GenerateRandomNumbers(t *testing.T) {
-	newService := New()
+func Test_Service_CreateNMax_GenerateRandomNumbers(t *testing.T) {
+	var err error
+
+	var randomService Service
+	{
+		randomConfig := DefaultConfig()
+		randomService, err = New(randomConfig)
+		if err != nil {
+			panic(err)
+		}
+	}
 
 	n := 100
 	max := 10
 
-	newRandomNumbers, err := newService.CreateNMax(n, max)
+	newRandomNumbers, err := randomService.CreateNMax(n, max)
 	if err != nil {
 		t.Fatal("expected", nil, "got", err)
 	}
